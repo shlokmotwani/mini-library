b731eaa8e26edb713fb283e7885b30a9618f013e
Shlok Motwani <shlok.codes@gmail.com>
2024-04-28 18:28:26 +0530

    MVP

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const myLibrary = [];
+const addABookButton = document.querySelector("#add-button");
+const bookGrid = document.querySelector("#book-grid");
+bookGrid.style.cssText = `
+width: 100vw;
+background-color: rgba(3, 252, 173, 0.3);
+display: grid;
+grid-auto-flow: row;
+grid-template-columns: repeat(5, 1fr);`;
 
 function Book(title, author, pageCount, isRead) {
   // the constructor...
@@ -7,10 +15,9 @@ function Book(title, author, pageCount, isRead) {
   this.pageCount = pageCount;
   this.isRead = isRead;
 
-  this.getDetails = function(){
-    return `${this.title} ${this.author} ${this.pageCount} ${this.isRead}`
-  }
-
+  this.getDetails = function () {
+    return `${this.title} ${this.author} ${this.pageCount} ${this.isRead}`;
+  };
 }
 
 function addBookToLibrary(book) {
@@ -18,52 +25,81 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
-const addABookButton = document.querySelector("#add-button");
-const bookInput = document.querySelector("#book-title");
-const authorInput = document.querySelector("#book-author");
-const pageCountInput = document.querySelector("#page-count");
-const isReadInput = document.querySelector("#is-read");
-const submitButton = document.querySelector("#submit-button");
+function setUpForm() {
+  for(let i=0; i<myLibrary.length; i++){
+    loadCard(myLibrary[i]);
+  }
 
-submitButton.addEventListener("click", ()=>{
+  const dialog = document.querySelector("dialog");
+  const closeButton = document.querySelector("#close-button");
+
+  const bookInput = document.querySelector("#book-title");
+  const authorInput = document.querySelector("#book-author");
+  const pageCountInput = document.querySelector("#page-count");
+  const isReadInput = document.querySelector("#is-read");
+  const submitButton = document.querySelector("#submit-button");
+
+  submitButton.addEventListener("click", () => {
     let bookTitle = bookInput.value;
     let bookAuthor = authorInput.value;
     let pageCount = pageCountInput.value;
     let isRead = isReadInput.value;
 
-    const book = new Book(bookTitle,
-        bookAuthor,
-        pageCount,
-        isRead);
+    const book = new Book(bookTitle, bookAuthor, pageCount, isRead);
     addBookToLibrary(book);
 
-    for(let i=0; i<myLibrary.length; i++){
-        console.log(myLibrary[i].getDetails());
-    }
+    //reset textfields of dialog
+    bookInput.value = "";
+    authorInput.value = "";
+    pageCountInput.value = "";
+    isReadInput.value = "";
 
+    console.log(myLibrary);
     dialog.close();
-})
+    loadCard(book);
+  });
 
+  //show the form as a dialog
+  addABookButton.addEventListener("click", () => {
+    dialog.showModal();
+  });
 
-let fiveAMClub = new Book("The 5 AM Club",
-"Robin Sharma",
-350,
-true);
-
-myLibrary.push(fiveAMClub);
-
-for(let i=0; i<myLibrary.length; i++){
-    console.log(myLibrary[i].getDetails());
+  closeButton.addEventListener("click", () => {
+    dialog.close();
+  });
 }
 
-const dialog = document.querySelector("dialog");
-const closeButton = document.querySelector("#close-button");
+function loadCard(book){
+  
+    const bookCard = document.createElement("div");
+    bookCard.style.cssText = `
+    border: 2px solid black;
+    border-radius: 8px;
+    color: white;
+    font-size: 1.2rem;
+    width: 180px;
+    display: flex;
+    flex-direction: column;
+    padding: 10px;
+    margin: 20px;
+    align-items: center;`;
 
-//show the form as a dialog
-addABookButton.addEventListener("click", ()=>{
-    dialog.showModal();
-});
+    const bookTitle = document.createElement("p");
+    bookTitle.textContent = book.title;
+    bookCard.appendChild(bookTitle);
+    const bookAuthor = document.createElement("p");
+    bookAuthor.textContent = book.author;
+    bookCard.appendChild(bookAuthor);
+    const bookPageCount = document.createElement("p");
+    bookPageCount.textContent = book.pageCount;
+    bookCard.appendChild(bookPageCount);
+    const bookIsRead = document.createElement("p");
+    bookIsRead.textContent = book.isRead;
+    bookCard.appendChild(bookIsRead);
 
-closeButton.addEventListener("click", ()=>{
-    dialog.close();
-})
+    bookGrid.appendChild(bookCard);
+}
+
+// myLibrary.push(new Book("The 5AM Club", "Robin Sharma", 600, "Yes"));
+// myLibrary.push(new Book("Jaadugarni", "Surendra Mohan Pathak", 240, "No"));
+setUpForm();
